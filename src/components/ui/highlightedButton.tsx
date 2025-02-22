ed9e2bc306f0974aeccb6c9cd0a601a6e3f563c2
Nijaz Andelić <77886732+AndNijaz@users.noreply.github.com>
2025-02-22 06:10:19 +0100

    finished sidebar

--- src/components/ui/highlightedButton.tsx
+++ src/components/ui/highlightedButton.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { Button } from "./button";
 import { NavLink, useLocation } from "react-router";
-import { link } from "fs";
 
 interface HighlightedButtonProps {
   linkTo: string;
@@ -10,25 +9,27 @@ interface HighlightedButtonProps {
 
 function HighlightedButton({ linkTo, children }: HighlightedButtonProps) {
   const loc = useLocation();
-
   const isButtonActive = loc.pathname.includes(linkTo);
 
   return (
     <NavLink className="w-full" to={`/${linkTo}`}>
       <Button
         variant={isButtonActive ? "defaultLinear" : "higlightedGray"}
-        className={`${
-          isButtonActive
-            ? "flex w-9/12 "
-            : "w-16 shadow-[0px_3.67px_3px_#4B7644]"
-        } h-16 rounded-3xl p-0 `}
+        className={`h-16 rounded-3xl p-0 flex items-center justify-center transition-all duration-300 gap-0!
+          ${
+            isButtonActive ? "w-9/12" : "w-16 shadow-[0px_3.67px_3px_#4B7644]"
+          }`}
       >
         {React.cloneElement(children as React.ReactElement, {
           color: isButtonActive ? "#333" : "#3DFF94",
         })}
-        {isButtonActive && (
-          <p className="">{linkTo.at(0)?.toUpperCase() + linkTo.slice(1)}</p>
-        )}
+        <p
+          className={`transition-opacity duration-300 ${
+            isButtonActive ? "opacity-100 ml-2" : "opacity-0 w-0"
+          }`}
+        >
+          {linkTo.at(0)?.toUpperCase() + linkTo.slice(1)}
+        </p>
       </Button>
     </NavLink>
   );
--- src/components/ui/sidebar.tsx
+++ src/components/ui/sidebar.tsx
@@ -6,25 +6,40 @@ import monitoring from "../../assets/monitoring.svg";
 import rizla from "../../assets/rizla.svg";
 import tegla from "../../assets/tegla.svg";
 
-import { HiOutlineHome } from "react-icons/hi";
+import {
+  HiOutlineCog,
+  HiOutlineDesktopComputer,
+  HiOutlineHome,
+  HiOutlineNewspaper,
+  HiOutlineQuestionMarkCircle,
+  HiOutlineSparkles,
+} from "react-icons/hi";
 
 function Sidebar() {
   return (
-    <div className="w-3xs border-r-2 border-[#333] h-screen">
+    <div className="w-3xs h-screen flex flex-col ">
       <Logo />
       <div className="flex flex-col items-start justify-center space-y-8 mt-8 ml-2">
         <HighlightedButton linkTo="home">
           <HiOutlineHome className="size-6" />
-          {/* <img src={homeIcon} alt="home" /> */}
         </HighlightedButton>
         <HighlightedButton linkTo="monitoring">
-          <img src={monitoring} alt="monitoring" />
+          <HiOutlineDesktopComputer className="size-6" />
         </HighlightedButton>
         <HighlightedButton linkTo="rizla">
-          <img src={rizla} alt="rizla" />
+          <HiOutlineNewspaper className="size-6" />
         </HighlightedButton>
         <HighlightedButton linkTo="tegla">
-          <img src={tegla} alt="tegla" />
+          <HiOutlineSparkles className="size-6" />
+        </HighlightedButton>
+      </div>
+
+      <div className="mt-auto flex flex-col ml-2 space-y-8 mb-8">
+        <HighlightedButton linkTo="#">
+          <HiOutlineQuestionMarkCircle className="size-6" />
+        </HighlightedButton>
+        <HighlightedButton linkTo="#">
+          <HiOutlineCog className="size-6" />
         </HighlightedButton>
       </div>
     </div>
